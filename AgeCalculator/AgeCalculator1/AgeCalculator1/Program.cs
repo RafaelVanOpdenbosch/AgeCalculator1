7931bfaa1b3b1fd16a7cea0dea0612b296c24548
Atif <s107214@ap.be>
2020-09-28 13:23:00 +0200

    Main Code

---
+++ AgeCalculator/AgeCalculator1/AgeCalculator1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AgeCalculator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
