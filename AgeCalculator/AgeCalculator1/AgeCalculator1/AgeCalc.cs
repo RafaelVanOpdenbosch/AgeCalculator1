382904068d17a5f978eee74c0a4c8323026f73bd
Atif <s107214@ap.be>
2020-09-28 13:52:00 +0200

    Class AgeCalc

---
+++ AgeCalculator/AgeCalculator1/AgeCalculator1/AgeCalc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeCalculator1
+{
+    class AgeCalc
+    {
+        public DateTime current;
+        public DateTime birth;
+
+        int AgeYear = 0;
+        int AgeMonth = 0;
+        int AgeDay = 0;
+
+
+
+        public AgeCalc(DateTime Current, DateTime Birth)
+        {
+            current = Current;
+            birth = Birth;
+            AgeYear = Current.Year - Birth.Year;
+            AgeMonth = Current.Month - Birth.Month;
+            AgeDay = Current.Day - Birth.Day;
+            if ( AgeMonth == 0)
+            {
+                if(AgeDay == 0)
+                {
+                    AgeYear++;
+                }
+            }
+
+        }
+
+
+        public static void Calculating()
+        {
+
+        }
+    }
+}
